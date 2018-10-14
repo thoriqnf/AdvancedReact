d70afc281bb661a09a1c6045a8d332ab64b9e28d
thoriq <thoriqnfaizal@gmail.com>
2018-10-14 22:24:12 +0700

    Cleanup codes

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return[
-    { title: 'javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'},
-  ]
-}
